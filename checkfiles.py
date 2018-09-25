e41a1567d9c17ce16980ab913610956ccecbf6d0
Alexander <35576482+turboasm123@users.noreply.github.com>
2018-09-25 19:03:52 +0300

    Update checkfiles.py

--- checkfiles.py
+++ checkfiles.py
@@ -7,8 +7,8 @@ import os,subprocess
 #Пути: db_path - база данных, root_path - корень
 vk_session = vk_api.VkApi(token='4c051f5929af7e49245a5b3b7e6923744ec14e57073b9825d8b1b90d926fd0cef3cd88e3697d4bab2ebca')
 vk = vk_session.get_api()
-db_path = os.path.abspath("/home/ts/jmih/ebanardy_zhmihnulo_devel/images_db")
-root_path=str("/home/ts/jmih/ebanardy_zhmihnulo_devel/")
+root_path=os.getcwd()
+db_path = os.path.abspath(root_path+"/images_db")
 uid='2000000002'
 #Определение и вывод количества файлов
 
