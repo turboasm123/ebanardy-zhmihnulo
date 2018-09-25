16fb171a94618951b4581c5d7a16916029e19363
Alexander <35576482+turboasm123@users.noreply.github.com>
2018-09-25 19:02:30 +0300

    Fixed the hardcoded variables

--- Collector.sh
+++ Collector.sh
@@ -1,7 +1,7 @@
 #Не запускать отдельно, только через Checker.py
 link=("http://www.funnycatsite.com/pictures/" "http://www.funnycatpix.com/")
 number=$(( $RANDOM % 2))
-db_path=/home/ts/jmih/ebanardy_zhmihnulo_devel/images_db
+db_path=${PWD}/images_db
 echo "Загрузка файлов через wget..."
 wget -r -A.jpg --random-wait -e robots=off  ${link[$number]} -nd -P $db_path
 python3 cleanup.py
