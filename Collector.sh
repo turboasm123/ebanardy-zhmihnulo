6ae388b7ad388ee65228bd5a2dcdfb70c9ddb217
Lucas <36301121+Lumanian@users.noreply.github.com>
2018-09-25 16:46:29 +0000

    Translated output

--- Collector.sh
+++ Collector.sh
@@ -1,7 +1,7 @@
-#Не запускать отдельно, только через Checker.py
+#Do not run separately, only through Checker.py
 link=("http://www.funnycatsite.com/pictures/" "http://www.funnycatpix.com/")
 number=$(( $RANDOM % 2))
 db_path=${PWD}/images_db
-echo "Загрузка файлов через wget..."
+echo "Downloading files through wget..."
 wget -r -A.jpg --random-wait -e robots=off  ${link[$number]} -nd -P $db_path
 python3 cleanup.py
