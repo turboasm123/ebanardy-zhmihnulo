1e8774998c6f75e54e87ebd1df3d5584001a04a6
Lucas <36301121+Lumanian@users.noreply.github.com>
2018-09-25 16:11:22 +0000

    Fixed hardcoded variables

--- porn.py
+++ porn.py
@@ -1,10 +1,12 @@
 import vk_api
 import json
-print("Pognali nahui postit znachit")
-vk_session = vk_api.VkApi(token='ec598549bc26fb8a45da242237175002c3ac8b83ac5f92dc5ac985fc98e115e4a377cf3f6ee55e856a6dd')
+print("I'm starting the publication in VK")
+vktoken = 'putyouraccounttokenhere'
+vk_session = vk_api.VkApi(token=vktoken)
 vk = vk_session.get_api()
 upload = vk_api.VkUpload(vk_session)
-uploadurl = upload.photo_wall('out/out.jpg', group_id=132966479)
+gid = 'putyourgroupidhere'
+uploadurl = upload.photo_wall('out/out.jpg', group_id=gid)
 print(uploadurl)
 print("=================")
 uploadurl = json.dumps(uploadurl)
@@ -13,5 +15,5 @@ uploadid = uploadres[0]['id']
 uploadown = uploadres[0]['owner_id']
 print(uploadid)
 print(uploadown)
-vk.wall.post(owner_id=-132966479, attachments="photo358134894_"+repr(uploadid), from_group=1, signed=1)
-print("Norm jmihnulo")
+vk.wall.post(owner_id="-"+repr(gid), attachments="photo"+repr(owner_id)"_"+repr(uploadid), from_group=1, signed=1)
+print("I'm done")
