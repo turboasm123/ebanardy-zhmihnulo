a0cb5212106b229a08be503f44188c42b03b3509
Lucas <36301121+Lumanian@users.noreply.github.com>
2018-09-26 09:19:01 +0000

    Output correction

--- Collector.sh
+++ Collector.sh
@@ -2,6 +2,6 @@
 link=("http://www.funnycatsite.com/pictures/" "http://www.funnycatpix.com/")
 number=$(( $RANDOM % 2))
 db_path=${PWD}/images_db
-echo "Downloading files through wget..."
+echo "Downloading files using wget..."
 wget -r -A.jpg --random-wait -e robots=off  ${link[$number]} -nd -P $db_path
 python3 cleanup.py
